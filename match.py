1e1760a9f0601f23bcc70dd8972fddbd28c8c52b
KUO <wangkuo0606@gmail.com>
2022-06-10 22:28:20 +0800

    match

---
+++ match.py
@@ -0,0 +1,24 @@
+import scipy.io as sio
+import os
+import numpy as np
+from tqdm import tqdm
+from func.cal_dist import cal_hamming, get_label
+from func.fig_plot import plot_roc
+featDir = './Data/QFIRE/Feat'
+maskDir = './Data/QFIRE/Mask'
+resultDir = './Result/QFIRE'
+if not os.path.isdir(resultDir):
+    os.makedirs(resultDir)
+iters = os.listdir(featDir)
+lg, lp, labelMat = get_label(maskDir, maskDir, 'bmp')
+labelMat[np.eye(len(lg),dtype=np.bool)] = 0 
+results = dict()
+for it in tqdm(iters):
+    featDirT = os.path.join(featDir, it)
+    scores, mrates = cal_hamming(featDirT, featDirT, maskDir, maskDir)
+    sio.savemat(os.path.join(resultDir, it+'_scores.mat'), {'scores':scores, 'labelMat':labelMat, 'mrates':mrates})
+    gg = np.array(scores[labelMat==1])
+    ii = np.array(scores[labelMat==-1])
+    result = plot_roc(gg, ii, os.path.join(resultDir, it+'_roc.png'))
+    results[it] = result
+    
