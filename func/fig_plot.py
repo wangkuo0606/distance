2478e1b2ee1a9d47b6489c0b375d7d86d5bb723a
KUO <wangkuo0606@gmail.com>
2022-06-10 19:31:12 +0800

    update

--- func/fig_plot.py
+++ func/fig_plot.py
@@ -1,7 +1,8 @@
 # (KUO) 2022/Jun/8
 import numpy as np
 import matplotlib.pyplot as plt
-def cal_far_frr(garr, iarr, resolu=2000):
+import os
+def cal_far_frr(garr, iarr, resolu=2500):
     d_max = max(garr.max(), iarr.max())
     d_min = min(garr.min(), iarr.min())
     far = np.empty(resolu)
@@ -11,13 +12,17 @@ def cal_far_frr(garr, iarr, resolu=2000):
         frr[i] = np.sum(garr > d) / garr.size
     return far, frr
 
-def cal_eer(garr, iarr):
+def cal_eer_tar(garr, iarr):
     far, frr = cal_far_frr(garr, iarr)
-    return far[np.argmin(np.abs(far - frr))]
+    result=dict()
+    result['eer'] = far[np.argmin(np.abs(far - frr))]
+    result['tar03'] = 1 - frr[np.argmin(np.abs(far - 0.001))]
+    result['tar04'] = 1- frr[np.argmin(np.abs(far - 0.0001))]
+    return result
 
-def plot_roc(garr, iarr, savefig=True):
+def plot_roc(garr, iarr, saveDir):
     euc_far, euc_frr = cal_far_frr(garr, iarr)
-    euc_eer = cal_eer(garr, iarr) 
+    euc_result = cal_eer_tar(garr, iarr) 
     plt.figure(figsize=(8, 6))
     plt.xscale('log')
     plt.title("Euclidean Distance ROC Curve")
@@ -27,5 +32,8 @@ def plot_roc(garr, iarr, savefig=True):
     plt.ylim([0, 1.0])
     plt.plot(euc_far, 1 - euc_frr, label="Euclidean")
     plt.legend()
-    plt.savefig('ROC.png')
-    print(euc_eer)
+    if not os.path.dirname(saveDir):
+        os.makedirs(os.path.dirname(saveDir))
+    plt.savefig(saveDir)
+    print(f"EER is {euc_result['eer']} and GAR is {euc_result['tar03']} and {euc_result['tar04']}")
+    return euc_result
