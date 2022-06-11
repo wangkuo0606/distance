e8b20bad8e2ea3fe4557f24a73dec951072cb5b1
KUO <wangkuo0606@gmail.com>
2022-06-11 17:21:56 +0800

    update

--- match.py
+++ match.py
@@ -6,9 +6,10 @@ from func.cal_dist import cal_hamming, get_label
 from func.fig_plot import plot_roc
 featDir = './Data/CASIA/Feat'
 maskDir = './Data/CASIA/Mask'
-resultDir = './Result/CASIA'
+resultDir = './Result/CASIA/U2'
 if not os.path.isdir(resultDir):
-    os.makedirs(resultDir)
+    os.makedirs(resultDir+'/score')
+    os.makedirs(resultDir+'/figure')
 iters = os.listdir(featDir)
 lg, lp, labelMat = get_label(maskDir, maskDir, 'bmp')
 labelMat[np.eye(len(lg),dtype=np.bool)] = 0 
@@ -16,9 +17,9 @@ results = dict()
 for it in tqdm(iters):
     featDirT = os.path.join(featDir, it)
     scores, mrates = cal_hamming(featDirT, featDirT, maskDir, maskDir)
-    sio.savemat(os.path.join(resultDir, it+'_scores.mat'), {'scores':scores, 'labelMat':labelMat, 'mrates':mrates})
+    sio.savemat(os.path.join(resultDir, 'score', it+'_scores.mat'), {'scores':scores, 'labelMat':labelMat, 'mrates':mrates})
     gg = np.array(scores[labelMat==1])
     ii = np.array(scores[labelMat==-1])
-    result = plot_roc(gg, ii, os.path.join(resultDir, it+'_roc.png'))
+    result = plot_roc(gg, ii, os.path.join(resultDir, 'figure', it+'_roc.png'))
     results[it] = result
-sio.savemat('result.mat', {'results':results})
+sio.savemat(os.path.join(resultDir, 'result.mat'), {'results':results})
